586b41219a86671163d888cb55756636884f9269
742314100 <742314100@qq.com>
2020-12-22 16:13:19 +0800

    ui-按钮

---
+++ ui/Assets/Scripts/MyGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyGame : MonoBehaviour {
+
+    public InputField userField; //指向了用户名下面的InputField组件
+
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Login()
+    {
+        string user = userField.text;
+        Debug.Log("登录中。。。"+user);
+    }
+
+}
---
+++ ui/Assets/Scripts/okBtn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class okBtn : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("down");
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("up");
+        transform.localScale = new Vector3(1.2f,1.2f,1.2f);
+    }
+}
