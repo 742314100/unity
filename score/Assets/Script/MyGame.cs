5e575c02bb6e9b68ac1c7d7f1fd68bf83e11c010
742314100 <742314100@qq.com>
2020-12-23 10:26:10 +0800

    3d

---
+++ score/Assets/Script/MyGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyGame : MonoBehaviour {
+
+    public int score;
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore()
+    {
+        score += 1;
+        scoreText.text = "" + score;
+    }
+}
