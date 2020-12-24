c7e74a05a3663dc7f988bfb5069bd67cecbec2a7
742314100 <742314100@qq.com>
2020-12-24 17:06:11 +0800

    小球碰撞游戏

--- roll a ball/Assets/Scripts/Player.cs
+++ roll a ball/Assets/Scripts/Player.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     private Rigidbody rd;
 
     public int force = 5;
+
+    private int score = 0;
+
+    public Text text; //游戏物体中的text
+
+    public GameObject winText; //整个游戏物体
 
 	// Use this for initialization
 	void Start () {
@@ -20,4 +27,31 @@ public class Player : MonoBehaviour {
 
         rd.AddForce(new Vector3(h, 0, v) * force); //按键控制移动
 	}
+    //碰撞检测
+    void OnCollisionEnter(Collision collision)
+    {
+        //collision.collider //获取碰撞到的游戏物体身上的Collision组件
+        //string name = collision.collider.name;
+        //print(name);
+        if (collision.collider.tag == "PickUp")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    //触发检测
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "PickUp")
+        {
+            score++;
+            text.text = score.ToString();
+            if(score == 8)
+            {
+                winText.SetActive(true);
+            }
+            Destroy(collider.gameObject);
+        }
+    }
+
 }
