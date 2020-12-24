44a4617e0911de85e63943650ba2eb4982811aeb
742314100 <742314100@qq.com>
2020-12-24 11:03:27 +0800

    小球跟随摄像机

---
+++ roll a ball/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform playerTransform;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - playerTransform.position; //摄像机位移-玩家位移
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTransform.position + offset; //给当前相机位置赋值，可以跟随玩家
+	}
+}
---
+++ roll a ball/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rd;
+
+    public int force = 5;
+
+	// Use this for initialization
+	void Start () {
+		rd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal"); //水平
+        float v = Input.GetAxis("Vertical"); //垂直
+
+        rd.AddForce(new Vector3(h, 0, v) * force); //按键控制移动
+	}
+}
