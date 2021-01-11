2815ad694e0bf6000e25900c0e294d311519547b
liweids <18730640692@139.com>
2021-01-11 16:44:05 +0800

    控制摄像机

---
+++ woyaozixue/Assets/script/sj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sj : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public int speed = 5;
+
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        float y = Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        transform.Translate(x,0,y);
+	}
+}
