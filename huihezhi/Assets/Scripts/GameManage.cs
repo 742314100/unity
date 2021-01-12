2b6295089de2f4d8a61ad535554c9a7496e5e03f
liweids <18730640692@139.com>
2021-01-12 16:29:51 +0800

    回合制

---
+++ huihezhi/Assets/Scripts/GameManage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour {
+    private GameObject[] players;
+    private GameObject[] enemys;
+    private List<GameObject> player;
+    private List<GameObject> enemy;
+    private List<GameObject> team; //当前是哪一队在操作
+    private int index;
+
+    public List<GameObject> GetPlayer
+    {
+        set { player = value; }
+        get { return player; }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        player = new List<GameObject>();
+        enemy = new List<GameObject>();
+        foreach (var o in players)
+        {
+            player.Add(o);
+        }
+        foreach (var o in enemys)
+        {
+            enemy.Add(o);
+        }
+
+        team = player;
+        index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Announce(GameObject go)
+    {
+        team[index].GetComponent<ObjectControl>().GetMove = true; 
+    }
+
+}
---
+++ huihezhi/Assets/Scripts/ObjectControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : BaseObj {
+
+    private Vector3 primitivePos;
+    private Vector3 primitiveRot;
+    private Vector3 temPos;
+    private bool canMove;
+
+    private Vector3 targetPos;
+    private float distance;
+
+    public bool GetMove
+    {
+        set { canMove = value; }
+        get { return canMove; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        distance = 1;
+        canMove = false;
+        primitivePos = transform.position;
+        primitiveRot = transform.eulerAngles;
+        temPos = Vector3.zero;
+        targetPos = new Vector3(-4,0.5f,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(canMove)
+        {
+            MoveTo(targetPos, distance);
+        }
+	}
+
+    protected override void MoveTo(Vector3 go, float dis)
+    {
+        if (Vector3.Distance(transform.position, go) > dis)
+        {
+            temPos = go;
+            temPos.y = transform.position.y;
+            transform.LookAt(temPos);
+            transform.Translate(Vector3.forward * Time.deltaTime * 5);
+        }
+        else 
+        {
+            canMove = false;
+            if(dis > 0.1f)
+            {
+            //攻击
+
+                //返回阶段的参数变化
+                GoBackParameter();
+            }
+            else
+            {
+            //返回原始位置
+                GoBack();
+            }
+        }
+    }
+
+    private void GoBack()
+    {
+        transform.position = primitivePos;
+        transform.eulerAngles = primitiveRot;
+        canMove = false;
+    }
+
+    private void GoBackParameter()
+    {
+        canMove = true;
+        targetPos = primitivePos;
+        distance = 0.1f;
+    }
+
+    private void ChangeNext
+    { 
+    
+    }
+
+}
